e53c28e64145ff373cf81bada307b3cba314ac6a
SydMontague <sydmontague@phoenix-staffel.de>
2024-01-04 18:05:57 +0100

    Fix build
    ... hopefully

--- src/main/java/net/digimonworld/decode/randomizer/settings/PatchSettings.java
+++ src/main/java/net/digimonworld/decode/randomizer/settings/PatchSettings.java
@@ -3,6 +3,7 @@ package net.digimonworld.decode.randomizer.settings;
 import java.awt.image.BufferedImage;
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Locale;
